6ac0a263e4d394765c8af237960b4556af174321
МАРК НЕМИРОВИЧ <nemirovich.m@misis.ru>
2024-11-12 23:14:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBI_reexam_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var structureTask = new Task1();
+            var classTask = new Task2();
+            var stringTask = new Task3();
+            var fileTask = new Task4();
+        }
+    }
+}
---
+++ Task4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBI_reexam_2
+{
+    internal class Task4
+    {
+        public Task4()
+        {
+
+        }
+    }
+}
